997e93299d3ff4fc9705577c255cdaa7d81d41b4
Gaurav <gaurav.baweja2508@gmail.com>
2020-10-25 11:10:08 +0530

    Comments added

--- Least_Square_Aprox_Deg1.m
+++ Least_Square_Aprox_Deg1.m
@@ -1,22 +1,26 @@
 %Least Square Approximation for degree one
-% n = input("Enter number of points n: ");
-% for (i=1:n)
-%     fprintf("Enter value of x(%d): ",i);
-%     x(i) = input('');
-%     fprintf("Enter value of y(%d): ",i);
-%     y(i) = input('');
-% end
+%For user input, Uncomment below code and Comment sample input
+%{
+n = input("Enter number of points n: ");
+for (i=1:n)
+    fprintf("Enter value of x(%d): ",i);
+    x(i) = input('');
+    fprintf("Enter value of y(%d): ",i);
+    y(i) = input('');
+end
+%}
 syms z;
-n = 10;
-x=[1,2,3,4,5,6,7,8,9,10];
-y=[1.3,3.5,4.2,5,7,8.8,10.1,12.5,13,15.6];
-sum_x = sum(x);
+%Sample input
+n = 10;                                     %Number of data inputs
+x=[1,2,3,4,5,6,7,8,9,10];                   %x input in array
+y=[1.3,3.5,4.2,5,7,8.8,10.1,12.5,13,15.6];  %f(x) input in array
+sum_x = sum(x);                             
 sum_y = sum(y);
 sum_xy = sum(x.*y);
 sum_x_sqr = sum(x.^2);
 A = [n, sum_x; sum_x, sum_x_sqr];
 B = [sum_y; sum_xy];
-X = inv(A)*B;
-f(z) = (X(1)+X(2)*z);
+X = inv(A)*B;                               %Taking inverse of A*X=B
+f(z) = (X(1)+X(2)*z);                       %Generalised poly function 
 fprintf("Poly function: P(x) = %.4f + %.4f*x\n",X(1),X(2));
 
